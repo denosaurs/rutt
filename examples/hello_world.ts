dbfef0aa1ac2b4b7dd80a0a3183578d4e33ccf35
Dean Srebnik <49134864+load1n9@users.noreply.github.com>
2023-01-23 17:33:06 -0500

    chore: bump std to 0.173.0 (#6)

--- examples/hello_world.ts
+++ examples/hello_world.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.173.0/http/server.ts";
 import { router } from "https://deno.land/x/rutt/mod.ts";
 
 await serve(
